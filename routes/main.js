15023b047a6a91de3dd60e58eb79bc65a29ce957
ha <abraes@mail.com>
2018-07-09 09:20:42 +0200

    correct template

--- routes/main.js
+++ routes/main.js
@@ -16,20 +16,6 @@ router.route('/about')
     res.render('main/about');
 });
 
-router.get('/marketing', (req, res, next) => {
-    res.render('main/marketing');
-});
 
-router.get('/compras-conjuntas-marketing-publicidad', (req, res, next) => {
-  res.render('cashflow/compras-conjuntas-marketing-publicidad');
-});
-
-router.get('/intercambios-recursos-marketing', (req, res, next) => {
-  res.render('cashflow/intercambios-recursos-marketing');
-});
-
-router.get('/venta-recursos-asociados', (req, res, next) => {
-  res.render('cashflow/venta-recursos-asociados');
-});
 
 module.exports = router;
